ad5c084860d3fe1f9ebaa644631c9df51de8905d
Joe Buckle <joe@white-fire.co.uk>
2019-12-13 17:29:19 +0000

    Updates

---
+++ precache-manifest.6ffa05b43319a8b26340a95649cb5674.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354f01bb2ff6e3286065899cec4d7ddd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30c2582da4613e30fe49",
+    "url": "/static/css/main.3ae99373.chunk.css"
+  },
+  {
+    "revision": "215ebb1cfa7beac9994d",
+    "url": "/static/js/2.b24421df.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b24421df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30c2582da4613e30fe49",
+    "url": "/static/js/main.bca85299.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
